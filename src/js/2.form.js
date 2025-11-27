04d5125ebffa2a187fbabc1071e7d4245d8e82d7
Ihor Kuzmenko <nameless1306@gmail.com>
2025-11-27 15:22:16 +0200

    my ninth js-hw

---
+++ src/js/2.form.js
@@ -0,0 +1,38 @@
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const inputEmail = form.elements.email;
+const inputMessage = form.elements.message;
+
+const savedData = localStorage.getItem('feedback-form-state');
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  inputEmail.value = formData.email;
+  inputMessage.value = formData.message;
+}
+
+form.addEventListener('input', () => {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (inputEmail.value === '' || inputMessage.value === '') {
+    return alert('Fill please all fields');
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+});
